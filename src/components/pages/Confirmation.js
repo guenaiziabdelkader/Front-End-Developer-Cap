9f2a803dc4a251b03000b1dbb3ca45cd37c00221
guenaiziabdelkader <111635911+guenaiziabdelkader@users.noreply.github.com>
2023-06-25 13:38:45 +0100

    Add files via upload

---
+++ src/components/pages/Confirmation.js
@@ -0,0 +1,33 @@
+import HeroImage from "../../assets/food/food5.jpeg";
+import { Link } from "react-router-dom";
+export default function Confirmation() {
+  return (
+    <header className="confirmation-header">
+      <img
+        className="confirmation-image"
+        src={HeroImage}
+        alt="Little Lemon Ingredients"
+      ></img>
+      <section className="reserve-header-text">
+        <h1>Your Reservation is Confirmed!</h1>
+      </section>
+
+      <section className="redirect-buttons">
+        <a
+          className="redirect-button"
+          href={require("../../assets/menu.webp")}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Browse Menu
+        </a>
+        <Link className="redirect-button" to="/order">
+          Order Online
+        </Link>
+        <Link className="redirect-button" to="/">
+          Home Page
+        </Link>
+      </section>
+    </header>
+  );
+}
